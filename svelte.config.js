50aed6ba341c82bc9c53d7048d0585a3b87435a5
Reinier Kip <dev@rjkip.nl>
2021-07-11 09:13:57 +1200

    Upgrade SvelteKit, install Netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,23 @@
-const node = require("@sveltejs/adapter-node");
-const pkg = require("./package.json");
+import node from "@sveltejs/adapter-node";
+import netlify from "@sveltejs/adapter-netlify";
 
 /** @type {import('@sveltejs/kit').Config} */
-module.exports = {
+const config = {
   kit: {
     // By default, `npm run build` will create a standard Node app.
     // You can create optimized builds for different platforms by
     // specifying a different adapter
-    adapter: node(),
+    adapter: netlify(),
 
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
 
     vite: {
       ssr: {
-        noExternal: Object.keys(pkg.dependencies || {}),
+        noExternal: ['@smui/button'],
       },
     },
   },
 };
+
+export default config;
